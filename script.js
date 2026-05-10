15bd689c6669f596605a16f0c978a13c67fa458a
Kunal11102002 <bhardwajkunal0111@gmail.com>
2026-05-10 14:32:30 +0530

    new commit

--- script.js
+++ script.js
@@ -43,3 +43,34 @@ navAnimation()
 
 
 
+function page2Animation() {
+    var rightElems = document.querySelectorAll(".right-elem")
+
+    rightElems.forEach(function (elem) {
+        elem.addEventListener("mouseenter", function () {
+
+
+
+
+            gsap.to(elem.childNodes[3], {
+                opacity: 1,
+                scale: 1
+            })
+        })
+        elem.addEventListener("mouseleave", function () {
+            gsap.to(elem.childNodes[3], {
+                opacity: 0,
+                scale: 0
+            })
+        })
+        elem.addEventListener("mousemove", function (dets) {
+
+            gsap.to(elem.childNodes[3], {
+                x: dets.x - elem.getBoundingClientRect().x - 90,
+                y: dets.y - elem.getBoundingClientRect().y - 215
+                // getBoundingClientRect() method use to get the position and size of an element on the screen
+            })
+        })
+    })
+}
+page2Animation()
